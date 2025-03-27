17376a21ea2188afe5a87a7dba4b40193ba6ece0
Administrator <admin@example.com>
2025-03-27 02:55:10 +0000

    增加新文件

---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+author_line=$1
+CI_COMMIT_MESSAGE=$2
+CI_COMMIT_SHA=$3
+CHANGED_FILES=$4
+
+
+
+# 遍历数组
+for file in "${CHANGED_FILES[@]}"; do
+  # 转义变量中的特殊字符（如双引号）
+  safe_commit_message=$(echo "$CI_COMMIT_MESSAGE" | sed 's/"/\\"/g')
+  safe_file_content=$(cat "$file" | sed 's/"/\\"/g' | tr '\n' ' ')
+
+  # 正确闭合 JSON 结构
+  curl -X POST \
+    http://192.168.21.192:8866/bigData/webhook/commit \
+    -H "Content-Type: application/json" \
+    -d "{
+      \"commitBy\": \"$author_line\",
+      \"commitInfo\": \"$safe_commit_message\",
+      \"fileInfos\": {
+      \"fileName\": \"$file\",
+      \"content\": \"$safe_file_content\"
+      }
+    }"  # 闭合 JSON 和引号
+  done
