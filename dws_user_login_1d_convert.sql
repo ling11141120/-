190acd7d839d69dd81257bae5f51e0d49a32d64a
192011 <192011@cd.com>
2025-04-02 11:38:18 +0800

    测试提交123

--- dws_user_login_1d_convert.sql
+++ dws_user_login_1d_convert.sql
@@ -7,6 +7,6 @@ left join
 dim.dim_user_accountinfo_df b
  on a.productid=b.productid and a.userid=b.userid
  where a.dt >='${today}' and a.dt<= date(date_add('${today}',interval 1 day ))
-group by 1,2,3,4,5,6,7,8,9,10,11,12
+group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14
 ;
-123123
+
