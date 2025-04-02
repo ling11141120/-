b2dc56f3bbeefaafb4122a6664887f2bbfd1de09
192011 <192011@cd.com>
2025-04-02 21:37:18 +0800

    测试提交123213123123123123123123124

--- dws_user_login_1d_convert.sql
+++ dws_user_login_1d_convert.sql
@@ -6,7 +6,7 @@ dwd.dwd_user_log_appstartlog a
 left join
 dim.dim_user_accountinfo_df b
  on a.productid=b.productid and a.userid=b.userid
- where a.dt >='${today}' and a.dt<= date(date_add('${today}',interval 1 day ))
+ where a.dt >='${today}' and a.dt<= date(date_add('${today}',interval 50 day ))
 group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14
 ;
 
