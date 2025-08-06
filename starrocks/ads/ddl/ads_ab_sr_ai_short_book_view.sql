9c5a150c834317d66ed6cd3dba9584cd73e651ba
050239 <050239@mail.cd>
2025-08-06 13:51:46 +0800

    RTM-9553 增加ods和ads视图ddl

---
+++ starrocks/ads/ddl/ads_ab_sr_ai_short_book_view.sql
@@ -0,0 +1,18 @@
+----------------------------------------------------------------
+-- 目标表： ads.ads_ab_sr_ai_short_book_view
+-- 功能： 海阅大模型短篇书籍AB测试视图
+-- 负责人： qhr
+-- 开发日期： 2023-08-05
+----------------------------------------------------------------
+
+create or replace view if not exists ads.ads_ab_sr_ai_short_book_view (
+     book_id       comment '书籍id'
+    ,version_cd    comment '版本号'
+)
+comment '海阅大模型短篇书籍AB测试视图'
+as
+select SwBookId*1000+LangId    as book_id
+      ,Version                 as version_cd
+  from ods.ods_shuangwen_tidb_en_shortstorybook
+ where SwBookId is not null
+;
