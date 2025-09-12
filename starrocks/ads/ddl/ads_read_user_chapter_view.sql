bd7c64757ebf521d90c75c6db4b8bf8cb7458d14
王曦 <w17680412316@.163.com>
2025-09-12 14:21:11 +0800

    引入项目相关生产代码v1

--- starrocks/ads/ddl/ads_read_user_chapter_view.sql
+++ starrocks/ads/ddl/ads_read_user_chapter_view.sql
@@ -1,30 +1,32 @@
 CREATE VIEW `ads_read_user_chapter_view` (
-    `dt` COMMENT "createtime 分区"
-  ,`product_id` COMMENT "产品id", 
-  `id` COMMENT "自增id", `book_id` COMMENT "书籍id"
-  , `chapter_id` COMMENT "章节id"
-  , `user_id` COMMENT "用户id"
-  , `prod_id` COMMENT "x值（没有用）"
-  , `create_time` COMMENT "阅读时间"
-  , `appid` COMMENT "应用程序id"
-  , `read_times` COMMENT "阅读时长（秒）-没有用")
+     `dt`          COMMENT    "createtime 分区"
+    ,`product_id`  COMMENT    "产品id"
+    ,`id`          COMMENT    "自增id"
+    ,`book_id`     COMMENT    "书籍id"
+    ,`chapter_id`  COMMENT    "章节id"
+    ,`user_id`     COMMENT    "用户id"
+    ,`prod_id`     COMMENT    "x值（没有用）"
+    ,`create_time` COMMENT    "阅读时间"
+    ,`appid`       COMMENT    "应用程序id"
+    ,`read_times`  COMMENT    "阅读时长（秒）-没有用")
 COMMENT "书籍章节阅读记录" 
 AS SELECT `ods_log`.`ods_book_user_readchapter`.`dt`
-        , `ods_log`.`ods_book_user_readchapter`.`productid` AS `product_id`
+        , `ods_log`.`ods_book_user_readchapter`.`productid`      AS `product_id`
         , `ods_log`.`ods_book_user_readchapter`.`id`
         , if((`ods_log`.`ods_book_user_readchapter`.`bookid` IS NULL) OR (`ods_log`.`ods_book_user_readchapter`.`bookid` = '')
         , -99
-        , `ods_log`.`ods_book_user_readchapter`.`bookid`) AS `book_id`
+        , `ods_log`.`ods_book_user_readchapter`.`bookid`)        AS `book_id`
         , if((`ods_log`.`ods_book_user_readchapter`.`chapterid` IS NULL) OR (`ods_log`.`ods_book_user_readchapter`.`chapterid` = '')
         , -99
-        , `ods_log`.`ods_book_user_readchapter`.`chapterid`) AS `chapter_id`
-        , `ods_log`.`ods_book_user_readchapter`.`userid` AS `user_id`
+        , `ods_log`.`ods_book_user_readchapter`.`chapterid`)     AS `chapter_id`
+        , `ods_log`.`ods_book_user_readchapter`.`userid`         AS `user_id`
         , if((`ods_log`.`ods_book_user_readchapter`.`prodid` IS NULL) OR (`ods_log`.`ods_book_user_readchapter`.`prodid` = '')
         , -99
-        , `ods_log`.`ods_book_user_readchapter`.`prodid`) AS `prod_id`
-        , `ods_log`.`ods_book_user_readchapter`.`createtime` AS `create_time`
+        , `ods_log`.`ods_book_user_readchapter`.`prodid`)        AS `prod_id`
+        , `ods_log`.`ods_book_user_readchapter`.`createtime`     AS `create_time`
         , if((`ods_log`.`ods_book_user_readchapter`.`appid` IS NULL) OR (`ods_log`.`ods_book_user_readchapter`.`appid` = '')
         , -99
-        , `ods_log`.`ods_book_user_readchapter`.`appid`) AS `appid`
-        , `ods_log`.`ods_book_user_readchapter`.`time` AS `read_times`
-FROM `ods_log`.`ods_book_user_readchapter`;
+        , `ods_log`.`ods_book_user_readchapter`.`appid`)         AS `appid`
+        , `ods_log`.`ods_book_user_readchapter`.`time`           AS `read_times`
+     FROM `ods_log`.`ods_book_user_readchapter`
+     ;
--- starrocks/ads/dml/P_ads_bi_user_read_book_info.sql
+++ starrocks/ads/dml/P_ads_bi_user_read_book_info.sql
@@ -7,31 +7,33 @@
 ----------------------------------------------------------------
 
 insert into ads.ads_bi_user_read_book_info
-select a.dt
-      ,a.product_id
-      ,a.user_id
-      ,a.book_id
-      ,a.site_id
-      ,a.corever
-      ,a.mt
-      ,case when d.user_id is not null then 1 else 0 end as is_channel_book 
-      ,now() as etl_time
-from dws.dws_read_user_readbook_ed a 
-left join (select Product_Id 
-                 ,user_id
-                 ,mt
-                 ,corever
-                 ,lang2 
-                 ,last_bookid
-            from dws.dws_user_market_channel_info_detail_td   
-           where dt='${bf_1_dt}' 
-             and last_bookid >0 ) d 
-on a.product_id =d.product_id  
-and a.book_id =d.last_bookid  
-and a.user_id =d.user_id 
-and a.mt=d.mt  
-and a.corever=d.corever   
-and a.current_language2=d.lang2 
-where a.dt>='${bf_1_dt}'
-and a.dt<'${dt}' 
-and a.product_id not in (8888,7777,3399);
+     select a.dt
+           ,a.product_id
+           ,a.user_id
+           ,a.book_id
+           ,a.site_id
+           ,a.corever
+           ,a.mt
+           ,case when d.user_id is not null then 1 
+                 else 0 
+            end    as is_channel_book 
+           ,now()  as etl_time
+       from dws.dws_read_user_readbook_ed a 
+       left join (select Product_Id 
+                        ,user_id
+                        ,mt
+                        ,corever
+                        ,lang2 
+                        ,last_bookid
+                  from dws.dws_user_market_channel_info_detail_td
+                  where dt='${bf_1_dt}' 
+                  and last_bookid >0 ) d 
+         on a.product_id =d.product_id  
+        and a.book_id =d.last_bookid  
+        and a.user_id =d.user_id 
+        and a.mt=d.mt  
+        and a.corever=d.corever   
+        and a.current_language2=d.lang2 
+      where a.dt>='${bf_1_dt}'
+        and a.dt<'${dt}' 
+        and a.product_id not in (8888,7777,3399);
