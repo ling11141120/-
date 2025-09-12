d71f79e5df03067d30498b318dcddc95a27919a0
王曦 <w17680412316@.163.com>
2025-09-12 15:32:22 +0800

    规范代码格式

--- starrocks/ads/ddl/ads_bi_user_book_consume_info.sql
+++ starrocks/ads/ddl/ads_bi_user_book_consume_info.sql
@@ -1,39 +1,40 @@
-CREATE TABLE `ads_bi_user_book_consume_info` (
-    `dt`                    date         NOT NULL   COMMENT "事件分区",
-    `product_id`            int(11)      NULL       COMMENT "产品id",
-    `book_id`               bigint(20)   NULL       COMMENT "书籍id",
-    `book_name`             varchar(512) NULL       COMMENT "书名",
-    `book_nature`           int(11)      NULL       COMMENT "书籍来源",
-    `new_cname`             varchar(512) NULL       COMMENT "书籍分类",
-    `build_time`            datetime     NULL       COMMENT "上架时间",
-    `normal_chapter_num_f`  int(11)      NULL       COMMENT "发布章节数",
-    `user_id`               bigint(20)   NULL       COMMENT "用户id",
-    `corever`               int(11)      NULL       COMMENT "corever",
-    `types`                 int(11)      NULL       COMMENT "1：阅币，2:礼券 3：赠送币 4：vip",
-    `amount`                int(11)      NULL       COMMENT "消耗数量",
-    `con_chapter_nums`      int(11)      NULL       COMMENT "消费章节数",
-    `is_read`               int(11)      NULL       COMMENT "1：有阅读 0 ：非阅读",
-    `is_channel_book`       int(11)      NULL       COMMENT "1：引流书籍 0：非引流书籍",
-    `etl_time`              datetime     NOT NULL   COMMENT "数据清洗时间"
-) ENGINE=OLAP 
-DUPLICATE KEY(`dt`, `product_id`)
+CREATE TABLE ads.ads_bi_user_book_consume_info (
+     dt                    date         NOT NULL   COMMENT "事件分区"
+    ,product_id            int(11)                 COMMENT "产品id"
+    ,book_id               bigint(20)              COMMENT "书籍id"
+    ,book_name             varchar(512)            COMMENT "书名"
+    ,book_nature           int(11)                 COMMENT "书籍来源"
+    ,new_cname             varchar(512)            COMMENT "书籍分类"
+    ,build_time            datetime                COMMENT "上架时间"
+    ,normal_chapter_num_f  int(11)                 COMMENT "发布章节数"
+    ,user_id               bigint(20)              COMMENT "用户id"
+    ,corever               int(11)                 COMMENT "corever"
+    ,types                 int(11)                 COMMENT "1：阅币，2:礼券 3：赠送币 4：vip"
+    ,amount                int(11)                 COMMENT "消耗数量"
+    ,con_chapter_nums      int(11)                 COMMENT "消费章节数"
+    ,is_read               int(11)                 COMMENT "1：有阅读 0 ：非阅读"
+    ,is_channel_book       int(11)                 COMMENT "1：引流书籍 0：非引流书籍"
+    ,etl_time              datetime     NOT NULL   COMMENT "数据清洗时间"
+)
+DUPLICATE KEY (dt, product_id)
 COMMENT "bi:用户消耗书籍数据"
-PARTITION BY RANGE(`dt`)
-DISTRIBUTED BY HASH(`dt`, `product_id`) BUCKETS 1 
-PROPERTIES ("replication_num" = "3",
-            "bloom_filter_columns" = "book_id",
-            "dynamic_partition.enable" = "true",
-            "dynamic_partition.time_unit" = "month",
-            "dynamic_partition.time_zone" = "Asia/Shanghai",
-            "dynamic_partition.start" = "-2147483648",
-            "dynamic_partition.end" = "3",
-            "dynamic_partition.prefix" = "p",
-            "dynamic_partition.buckets" = "1",
-            "dynamic_partition.history_partition_num" = "0",
-            "dynamic_partition.start_day_of_month" = "1",
-            "in_memory" = "false",
-            "enable_persistent_index" = "true",
-            "replicated_storage" = "true",
-            "compression" = "LZ4"
+PARTITION BY RANGE(dt)
+DISTRIBUTED BY HASH(dt, product_id) BUCKETS 1
+PROPERTIES (
+    "replication_num" = "3",
+    "bloom_filter_columns" = "book_id",
+    "dynamic_partition.enable" = "true",
+    "dynamic_partition.time_unit" = "month",
+    "dynamic_partition.time_zone" = "Asia/Shanghai",
+    "dynamic_partition.start" = "-2147483648",
+    "dynamic_partition.end" = "3",
+    "dynamic_partition.prefix" = "p",
+    "dynamic_partition.buckets" = "1",
+    "dynamic_partition.history_partition_num" = "0",
+    "dynamic_partition.start_day_of_month" = "1",
+    "in_memory" = "false",
+    "enable_persistent_index" = "true",
+    "replicated_storage" = "true",
+    "compression" = "LZ4",
 )
 ;
--- starrocks/ads/ddl/ads_bi_user_read_book_info.sql
+++ starrocks/ads/ddl/ads_bi_user_read_book_info.sql
@@ -1,32 +1,33 @@
-CREATE TABLE `ads_bi_user_read_book_info` (
-    `dt`              date           NOT NULL        COMMENT "createtime 分区",
-    `product_id`      int(11)        NOT NULL        COMMENT "产品id",
-    `user_id`         bigint(20)     NOT NULL        COMMENT "用户id",
-    `book_id`         bigint(20)     NOT NULL        COMMENT "书籍id",
-    `site_id`         int(11)        NULL            COMMENT "书籍语言id",
-    `corever`         int(11)        NULL            COMMENT "corever",
-    `is_channel_book` int(11)        NULL            COMMENT "是否引流书籍",
-    `etl_time`        datetime       NULL            COMMENT "etl时间",
-  INDEX index_product_id (`product_id`) USING BITMAP COMMENT '产品id索引'
+CREATE TABLE ads.ads_bi_user_read_book_info (
+     dt              date           NOT NULL          COMMENT "createtime 分区"
+    ,product_id      int(11)        NOT NULL          COMMENT "产品id"
+    ,user_id         bigint(20)     NOT NULL          COMMENT "用户id"
+    ,book_id         bigint(20)     NOT NULL          COMMENT "书籍id"
+    ,site_id         int(11)                          COMMENT "书籍语言id"
+    ,corever         int(11)                          COMMENT "corever"
+    ,is_channel_book int(11)                          COMMENT "是否引流书籍"
+    ,etl_time        datetime                         COMMENT "etl时间"
+INDEX index_product_id (product_id) USING BITMAP      COMMENT '产品id索引'
 )
-PRIMARY KEY (`dt`, `product_id`, `user_id`, `book_id`)
+PRIMARY KEY (dt, product_id, user_id, book_id)
 COMMENT "运营业务bi报表需求-阅读域用户粒度书籍阅读信息表"
-PARTITION BY RANGE (`dt`)
-DISTRIBUTED BY HASH (`product_id`, `user_id`, `book_id`) BUCKETS 1 
-PROPERTIES ("replication_num" = "3",
-            "bloom_filter_columns" = "user_id, book_id",
-            "dynamic_partition.enable" = "true",
-            "dynamic_partition.time_unit" = "month",
-            "dynamic_partition.time_zone" = "Asia/Shanghai",
-            "dynamic_partition.start" = "-2147483648",
-            "dynamic_partition.end" = "3",
-            "dynamic_partition.prefix" = "p",
-            "dynamic_partition.buckets" = "1",
-            "dynamic_partition.history_partition_num" = "0",
-            "dynamic_partition.start_day_of_month" = "1",
-            "in_memory" = "false",
-            "enable_persistent_index" = "true",
-            "replicated_storage" = "true",
-            "compression" = "LZ4"
+PARTITION BY RANGE (dt)
+DISTRIBUTED BY HASH (dt, product_id, user_id, book_id) BUCKETS 1
+PROPERTIES (
+    "replication_num" = "3",
+    "bloom_filter_columns" = "user_id, book_id",
+    "dynamic_partition.enable" = "true",
+    "dynamic_partition.time_unit" = "month",
+    "dynamic_partition.time_zone" = "Asia/Shanghai",
+    "dynamic_partition.start" = "-2147483648",
+    "dynamic_partition.end" = "3",
+    "dynamic_partition.prefix" = "p",
+    "dynamic_partition.buckets" = "1",
+    "dynamic_partition.history_partition_num" = "0",
+    "dynamic_partition.start_day_of_month" = "1",
+    "in_memory" = "false",
+    "enable_persistent_index" = "true",
+    "replicated_storage" = "true",
+    "compression" = "LZ4"
 )
 ;
