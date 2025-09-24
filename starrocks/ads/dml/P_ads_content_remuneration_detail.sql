bac958cd6df2b17cf9be09f3b3008d696b802d3d
025716 <025716@mail.cd>
2025-09-24 19:30:05 +0800

    Merge remote-tracking branch 'origin/dev+xjc' into dev+xjc
    提交人：谢季昌
    提交时间：2025-09-24 19:30:00
    提交内容：针对 ods_tidb_shuangwen_xx_objectchapter 表单，整理其数据血缘关系中涉及的所有表单的 DML 代码
    DML 处理表单列表：
    ads_content_book_chapter_interpreter_p_di
    ads_content_en_book_stat
    ads_content_remuneration_detail
    ads_mobotrans_capacity_author_resource
    ads_mobotrans_capacity_author_resource_manager

---
+++ starrocks/ads/dml/P_ads_content_remuneration_detail.sql
@@ -0,0 +1,47 @@
+----------------------------------------------------------------
+-- 程序功能： 实时稿酬
+-- 程序名： P_ads_content_remuneration_detail
+-- 目标表： ads.ads_content_remuneration_detail
+-- 负责人： xjc
+-- 开发日期：2025-09-24
+-- 版本号： v0.0.0
+----------------------------------------------------------------
+
+
+
+insert into ads.ads_content_remuneration_detail
+select md5(concat_ws('_'
+                     ,date(CreateTime)
+                     ,a.ToLanguage
+                     ,a.AuthorId
+                     ,a.RoleType
+                     ,b.PenName
+                     ,c.BookName
+                     ,d.ChapterName
+                    )
+          )                as md5_key       -- 唯一主键
+      ,date(CreateTime)    as dt            -- 统计日期（完成时间）
+      ,a.ToLanguage        as site_id       -- 语言ID
+      ,a.AuthorId          as author_id     -- 译员ID
+      ,a.RoleType          as role_type     -- 角色类型
+      ,b.PenName           as pen_name      -- 译名
+      ,a.bookId            as book_id       -- 书籍ID
+      ,c.BookName          as book_name     -- 书名
+      ,a.ChapterId         as chapter_id    -- 章节ID
+      ,d.ChapterName       as chapter_name  -- 章节名
+      ,now()               as etl_time      -- ETL时间
+  from ods.ods_edit_book_RemunerationDetail a
+  left join ods.ods_tidb_shuangwen_xx_objectauthor b
+         on a.productid = b.productid
+        and a.AuthorId = b.AccountId
+        and a.ToLanguage = b.ToLanguage
+  left join ods.ods_tidb_shuangwen_en_objectbook c
+         on a.productid = c.productid
+        and a.bookId = c.SwBookId
+        and a.ToLanguage = c.ToLanguage
+        and c.Status = 1
+  left join ods.ods_tidb_shuangwen_xx_objectchapter d
+         on a.productid = d.productid
+        and c.id = d.ObjectBookId
+        and a.ChapterId = d.Id
+;
---
+++ starrocks/ads/dml/P_ads_mobotrans_capacity_author_resource_manager.sql
@@ -0,0 +1,90 @@
+----------------------------------------------------------------
+-- 程序功能： 产能人才资源管理看板
+-- 程序名： P_ads_mobotrans_capacity_author_resource_manager
+-- 目标表： ads.ads_mobotrans_capacity_author_resource_manager
+-- 负责人： xjc
+-- 开发日期：2025-09-24
+-- 版本号： v0.0.0
+----------------------------------------------------------------
+
+with ca as (
+    select site_id
+          ,role_type
+          ,author_id
+          ,work_status
+          ,font_length_curmonth
+          ,avg_score
+      from ads.ads_mobotrans_capacity_author_resource
+     where dt = '${dt}'
+       and role_type in (1, 2, 3)
+)
+select  '${dt}'                                         as dt
+      ,site_id                                          as site_id
+      ,role_type                                        as role_type
+      ,total_num                                        as total_num
+      ,work_status_coo                                  as work_status_coo
+      ,work_status_bre                                  as work_status_bre
+      ,work_status_sle                                  as work_status_sle
+      ,active_num_curmonth                              as active_num_curmonth
+      ,active_num_curmonth/total_num                    as active_rate_curmonth
+      ,font_length_curmonth                             as font_length_curmonth
+      ,font_length_tar_curmonth                         as font_length_tar_curmonth
+      ,font_length_tar_curmonth/font_length_curmonth    as length_rate_tar_curmonth
+      ,font_length_par_curmonth                         as font_length_par_curmonth
+      ,font_length_par_curmonth/font_length_curmonth    as length_rate_par_curmonth
+      ,font_length2_curmonth - font_length_curmonth     as process_offset
+      ,score_a_num                                      as score_a_num
+      ,score_b_num                                      as score_b_num
+      ,score_c_num                                      as score_c_num
+      ,now()                                            as etl_time
+  from (
+        select site_id
+              ,role_type                                                                           as role_type
+              ,total_num                                                                           as total_num
+              ,work_status_coo                                                                     as work_status_coo
+              ,work_status_bre                                                                     as work_status_bre
+              ,work_status_sle                                                                     as work_status_sle
+              ,active_num_curmonth                                                                 as active_num_curmonth
+              ,active_num_curmonth/total_num                                                       as active_rate_curmonth
+              ,font_length_curmonth                                                                as font_length_curmonth
+              ,font_length_tar_curmonth                                                            as font_length_tar_curmonth
+              ,font_length_tar_curmonth/font_length_curmonth as length_rate_tar_curmonth
+              ,font_length_par_curmonth                                                            as font_length_par_curmonth
+              ,font_length_par_curmonth/font_length_curmonth as length_rate_par_curmonth
+              ,lag(font_length_curmonth, 1, null) over(partition by site_id order by role_type)    as font_length2_curmonth
+              ,score_a_num                                                                         as score_a_num
+              ,score_b_num                                                                         as score_b_num
+              ,score_c_num                                                                         as score_c_num
+          from (
+                  select site_id
+                        ,role_type                                                                                    as role_type
+                        ,count(distinct author_id)                                                                    as total_num
+                        ,count(distinct if(work_status = 3, author_id, null))                                         as work_status_coo
+                        ,count(distinct if(work_status = 1, author_id, null))                                         as work_status_bre
+                        ,count(distinct if(work_status = 2, author_id, null))                                         as work_status_sle
+                        ,count(distinct if(font_length_curmonth is not null
+                                           and font_length_curmonth != 0, author_id, null))                           as active_num_curmonth
+                        ,sum(if(font_length_curmonth is not null, font_length_curmonth, 0))                           as font_length_curmonth
+                        ,sum(if(font_length_curmonth is not null
+                                 and font_length_curmonth != 0 and AuthorId is not null, font_length_curmonth, 0))    as font_length_tar_curmonth
+                        ,sum(if(font_length_curmonth is not null
+                                 and font_length_curmonth != 0 and AuthorId is null, font_length_curmonth, 0))        as font_length_par_curmonth
+                        ,count(distinct if(avg_score is not null
+                                            and avg_score >= 3.4, author_id, null))                                   as score_a_num
+                        ,count(distinct if(avg_score is not null
+                                            and avg_score >= 3 and avg_score < 3.4, author_id, null))                 as score_b_num
+                        ,count(distinct if(avg_score is not null
+                                            and avg_score < 3, author_id, null))                                      as score_c_num
+                    from ca
+                    left join (select SiteId
+                                     ,AuthorId
+                                     ,RoleType
+                                 from ods.ods_tidb_shuangwen_en_viscauthorconfig
+                                where date_format(dt, '%Y-%m') = date_format('${bf_1_dt}', '%Y-%m')
+                              ) b
+                      on ca.site_id = b.SiteId
+                     and ca.author_id = b.AuthorId
+                     and ca.role_type = b.RoleType
+                   group by site_id, role_type
+                 ) t
+       ) s
