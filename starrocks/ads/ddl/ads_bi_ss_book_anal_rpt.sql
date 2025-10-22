a8bc98340b31161f9de986a3e2046618005beb82
王曦 <w17680412316@.163.com>
2025-10-22 15:06:43 +0800

    引入短篇书籍分析报表代码和更新支付漏斗报表

--- starrocks/ads/ddl/ads_bi_ss_book_anal_rpt.sql
+++ starrocks/ads/ddl/ads_bi_ss_book_anal_rpt.sql
@@ -20,27 +20,24 @@ CREATE TABLE ads.ads_bi_ss_book_anal_rpt (
     ,pub_wc            DECIMAL(10,0)          COMMENT '发布字数'
     ,ttl_chap_num      DECIMAL(4,0)           COMMENT '总章节数'
     ,pub_chap          DECIMAL(4,0)           COMMENT '发布章节'
-    ,book_cmp_stat     VARCHAR(50)            COMMENT '书籍完本状态'                ---新增字段
-    ,mat_is_cmp        INT                    COMMENT '物料是否齐全'
+    ,book_cmp_stat     VARCHAR(20)            COMMENT '书籍完本状态'
+    ,mat_is_cmp        VARCHAR(20)            COMMENT '物料是否齐全'
     ,ast_cmp_dt        DATETIME               COMMENT '素材完成日期'
     ,rev_sc_dt         DATETIME               COMMENT '审核抽查日期'
     ,zhtw_sig_ctr_dt   DATETIME               COMMENT '繁体签约日期'
     ,ph2_test_bgn_dt   DATETIME               COMMENT '第二阶段测试开始日期'
     ,ph1_test_bgn_dt   DATETIME               COMMENT '第一阶段测试开始日期'
     ,trl_cost_mon      DECIMAL(10,4)          COMMENT '本月翻译成本'
-    ,sign_ctr_cost_mon DECIMAL(10,4)          COMMENT '本月签约成本'              ---新增字段
-    ,ad_cost_mon       DECIMAL(10,4)          COMMENT '本月广告成本'              ---新增字段
-    ,ttl_cost          DECIMAL(10,4)          COMMENT '总成本'                   ---新增字段
-    ,d_rev             DECIMAL(10,4)          COMMENT '当日收入'                 ---新增字段
+    ,trl_cost          DECIMAL(10,4)          COMMENT '翻译成本'
+    ,sign_ctr_cost     DECIMAL(10,4)          COMMENT '签约成本'
+    ,ad_cost           DECIMAL(10,4)          COMMENT '广告成本'
     ,amt_mon           DECIMAL(10,4)          COMMENT '本月收入'
     ,amt_30d           DECIMAL(10,4)          COMMENT '近30天收入'
     ,amt_7d            DECIMAL(10,4)          COMMENT '近7天收入'
-    ,ttl_rev           DECIMAL(10,4)          COMMENT '总收入'                   ---新增字段
-    ,ttl_roi           DECIMAL(10,4)          COMMENT '总roi'                    ---新增字段
-    ,ded_delv_cost_roi DECIMAL(10,4)          COMMENT '扣除投放成本roi'           ---新增字段
+    ,d_rev             DECIMAL(10,4)          COMMENT '当日收入'
 )
 PRIMARY KEY (dt, product_id, book_id)
-COMMENT 'bi-短篇书籍分析报表'
+COMMENT 'bi-短篇书籍项目收益明细报表'
 PARTITION BY DATE_TRUNC('month', dt)
 DISTRIBUTED BY HASH(dt, product_id)
 PROPERTIES (
