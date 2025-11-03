6f587c9293e62d0434b60564c041d6de6983d405
王曦 <w17680412316@.163.com>
2025-11-03 14:29:15 +0800

    优化第三方支付漏斗SQL查询格式 调整SQL查询的缩进和格式以提高可读性，保持代码风格一致

--- starrocks/ads/dml/P_ads_sv_third_party_payment_funnel_recharge.sql
+++ starrocks/ads/dml/P_ads_sv_third_party_payment_funnel_recharge.sql
@@ -29,9 +29,9 @@ with active as(
        and dt >= '${bf_1_dt}'
        and dt <= '${dt}'
      group by 1, 2, 3, 4, 5, 6, 7
-),
+)
 -- 字典表
-dim_strategy as (
+, dim_strategy as (
     select Id                                              as id
           ,Name                                            as name
           ,max(StrategyCode)                               as strategy_code
@@ -50,78 +50,79 @@ dim_strategy as (
           ,null    as sort_popup
           ,null    as sort_return
     from ads.ads_sv_goods_strategy_view
-),
+)
 -- 订单表数据
-payorder as(select dt
-                  ,user_id
-                  ,case when mt=1 then 'iOS'
-                        when mt=4 then 'Android'
-                        else '其他'
-                    end               as mt
-                  ,corever2           as core
-                  ,base_amount/100    as base_amount
-                  ,order_id
-                  ,t1.create_time
-                  ,case when SPLIT(get_json_string(custom_data, '$.sendId'), '_')[1]='201300' then '商店页'
-                        when SPLIT(get_json_string(custom_data, '$.sendId'), '_')[1]='200900' then '半屏'
-                        when SPLIT(get_json_string(custom_data, '$.sendId'), '_')[1]='200800' and SPLIT(get_json_string(custom_data, '$.sendId'), '_')[2]='0' then '解锁页VIP'
-                        when SPLIT(get_json_string(custom_data, '$.sendId'), '_')[1]='203300' then 'H5' 
-                        when SPLIT(get_json_string(custom_data, '$.sendId'), '_')[1] is null
-                             then (case when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=202100 
-                                         and split(get_json_string(custom_data, '$.activityLink'), '_')[2] in (0,1) then '普通弹窗'
-                                        when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=202100
-                                         and split(get_json_string(custom_data, '$.activityLink'), '_')[2]=3 then '充值返回推弹窗'
-                                        when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=202100
-                                         and split(get_json_string(custom_data, '$.activityLink'), '_')[2] in (12) then '充值弹层'
-                                        when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=200800
-                                         and SPLIT(get_json_string(custom_data, '$.sendId'), '_')[2]='0' then '解锁页VIP'
-                                        when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=205000  then '会员中心页'
-                                        when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=203200 then '悬浮窗'
-                                        when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=204000 then 'TAB栏'
-                                        when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=204100 then 'banner'
-                                        when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=210010 then 'push'
-                                        when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=203600 then '开屏页'
-                                        when split(get_json_string(custom_data, '$.activityLink'), '_')[1] is null then '其他'
-                                        else split(get_json_string(custom_data, '$.activityLink'), '_')[1]
-                                    end
-                                  )
-                        when SPLIT(get_json_string(custom_data, '$.sendId'), '_')[1]='202100'
-                         and SPLIT(get_json_string(custom_data, '$.sendId'), '_')[2] in ('0','1') then '普通弹窗'
-                        when SPLIT(get_json_string(custom_data, '$.sendId'), '_')[1]='202100'
-                         and SPLIT(get_json_string(custom_data, '$.sendId'), '_')[2] in ('3') then '充值返回推弹窗'
-                        when SPLIT(get_json_string(custom_data, '$.sendId'), '_')[1]='202100'
-                         and SPLIT(get_json_string(custom_data, '$.sendId'), '_')[2] in ('12') then '充值弹层'
-                        else '其他'
-                    end                                                    as recharge_source
-                  ,get_json_string(custom_data, '$.strategyId')            as strategy_id
-                  ,get_json_string(cooorder_extinfo, '$.SubscribeStatus')  as SubscribeStatus
-                  ,case when shop_item = 0 then '普通充值'
-                        when shop_item = 810 then 'SVIP'
-                        when shop_item = 840 then '签到卡'
-                        else '其他'
-                    end                                                    as shop_item_type
-                  ,case when subpay_type = 'AppStore' then 'iOS'
-                        when subpay_type = 'GooglePlay' then '安卓'
-                        when subpay_type = 'AppGallery' then '华为'
-                        when subpay_type = 'MiGlobal' then '小米'
-                        else '三方'end as zfqd
-                  ,if(subpay_type in ('AppStore','GooglePlay','AppGallery','MiGlobal'), 0, 1) as if_third
-                  ,time_duration from (select *
-                                         from ads.ads_short_video_payorder_view
-                                        where test_flag=0
-                                          and product_id=6833
-                                          and test_flag = 0
-                                          and dt >= '${bf_1_dt}'
-                                          and dt <= '${dt}'
-                                      )         as t1
-                                 left join (select order_serial_id
-                                                  ,finish_time - create_time    as time_duration
-                                              from ads.ads_report_trade_hkpayorder_detail_view order_hk
-                                             where product_id = 6833
-                                               and dt >= '${bf_1_dt}'
-                                               and dt <= '${dt}'
-                                           )    as t2
-                                  on t1.order_id = t2.order_serial_id
+, payorder as(select dt
+                    ,user_id
+                    ,case when mt=1 then 'iOS'
+                          when mt=4 then 'Android'
+                          else '其他'
+                      end               as mt
+                    ,corever2           as core
+                    ,base_amount/100    as base_amount
+                    ,order_id
+                    ,t1.create_time
+                    ,case when SPLIT(get_json_string(custom_data, '$.sendId'), '_')[1]='201300' then '商店页'
+                          when SPLIT(get_json_string(custom_data, '$.sendId'), '_')[1]='200900' then '半屏'
+                          when SPLIT(get_json_string(custom_data, '$.sendId'), '_')[1]='200800' and SPLIT(get_json_string(custom_data, '$.sendId'), '_')[2]='0' then '解锁页VIP'
+                          when SPLIT(get_json_string(custom_data, '$.sendId'), '_')[1]='203300' then 'H5' 
+                          when SPLIT(get_json_string(custom_data, '$.sendId'), '_')[1] is null
+                               then (case when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=202100 
+                                           and split(get_json_string(custom_data, '$.activityLink'), '_')[2] in (0,1) then '普通弹窗'
+                                          when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=202100
+                                           and split(get_json_string(custom_data, '$.activityLink'), '_')[2]=3 then '充值返回推弹窗'
+                                          when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=202100
+                                           and split(get_json_string(custom_data, '$.activityLink'), '_')[2] in (12) then '充值弹层'
+                                          when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=200800
+                                           and SPLIT(get_json_string(custom_data, '$.sendId'), '_')[2]='0' then '解锁页VIP'
+                                          when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=205000  then '会员中心页'
+                                          when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=203200 then '悬浮窗'
+                                          when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=204000 then 'TAB栏'
+                                          when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=204100 then 'banner'
+                                          when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=210010 then 'push'
+                                          when split(get_json_string(custom_data, '$.activityLink'), '_')[1]=203600 then '开屏页'
+                                          when split(get_json_string(custom_data, '$.activityLink'), '_')[1] is null then '其他'
+                                          else split(get_json_string(custom_data, '$.activityLink'), '_')[1]
+                                      end
+                                    )
+                          when SPLIT(get_json_string(custom_data, '$.sendId'), '_')[1]='202100'
+                           and SPLIT(get_json_string(custom_data, '$.sendId'), '_')[2] in ('0','1') then '普通弹窗'
+                          when SPLIT(get_json_string(custom_data, '$.sendId'), '_')[1]='202100'
+                           and SPLIT(get_json_string(custom_data, '$.sendId'), '_')[2] in ('3') then '充值返回推弹窗'
+                          when SPLIT(get_json_string(custom_data, '$.sendId'), '_')[1]='202100'
+                           and SPLIT(get_json_string(custom_data, '$.sendId'), '_')[2] in ('12') then '充值弹层'
+                          else '其他'
+                      end                                                    as recharge_source
+                    ,get_json_string(custom_data, '$.strategyId')            as strategy_id
+                    ,get_json_string(cooorder_extinfo, '$.SubscribeStatus')  as SubscribeStatus
+                    ,case when shop_item = 0 then '普通充值'
+                          when shop_item = 810 then 'SVIP'
+                          when shop_item = 840 then '签到卡'
+                          else '其他'
+                      end                                                    as shop_item_type
+                    ,case when subpay_type = 'AppStore' then 'iOS'
+                          when subpay_type = 'GooglePlay' then '安卓'
+                          when subpay_type = 'AppGallery' then '华为'
+                          when subpay_type = 'MiGlobal' then '小米'
+                          else '三方'end as zfqd
+                    ,if(subpay_type in ('AppStore','GooglePlay','AppGallery','MiGlobal'), 0, 1) as if_third
+                    ,time_duration
+                from (select *
+                        from ads.ads_short_video_payorder_view
+                       where test_flag=0
+                         and product_id=6833
+                         and test_flag = 0
+                         and dt >= '${bf_1_dt}'
+                         and dt <= '${dt}'
+                     )         as t1
+                left join (select order_serial_id
+                                 ,finish_time - create_time    as time_duration
+                             from ads.ads_report_trade_hkpayorder_detail_view order_hk
+                            where product_id = 6833
+                              and dt >= '${bf_1_dt}'
+                              and dt <= '${dt}'
+                          )    as t2
+                 on t1.order_id = t2.order_serial_id
 )
 -- 活跃关联充值成功
 select payorder.dt
