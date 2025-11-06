c46a7d9777373d5c5ff5b51d6f0ca108787bbeda
王曦 <w17680412316@.163.com>
2025-11-06 15:16:02 +0800

    修正第五章留存率计算逻辑 调整SQL查询结构，将总阅读人数计算分离到独立子查询中 添加过滤条件排除特定site_id和sexy2值 修正留存率计算公式为第五章阅读人数/总阅读人数

--- starrocks/ads/dml/P_ads_bi_book_chap5_info.sql
+++ starrocks/ads/dml/P_ads_bi_book_chap5_info.sql
@@ -7,40 +7,53 @@
 -- 版本号： v0.0.0
 ----------------------------------------------------------------
 
-insert into ads.ads_bi_book_chap5_ret_rt_info (
-     dt                   -- 日期
-    ,book_id              -- 书籍id
-    ,site_id              -- 语言id
-    ,new_cid              -- 分类id
-    ,new_cname            -- 分类名称
-    ,chap5_read_num       -- 第五章阅读人数
-    ,ttl_chap_read_num    -- 总阅读人数
-    ,chap5_ret_rt         -- 第五章留存率
-)
+insert into ads.ads_bi_book_chap5_ret_rt_info
 with t1 as (
-    select '${bf_1_dt}' as dt
-          ,a1.bookid    as book_id
-          ,a2.serial_number as serial_number
-          ,max(a2.site_id) as site_id
-          ,max(a2.new_cid) as new_cid
-          ,max(a2.new_cname) as new_cname
-          ,count(distinct a1.userid) as chap5_read_num
-          ,count(distinct a1.userid) as ttl_chap_read_num
-      from ods_log.ods_book_user_readchapter     as a1
-      left join dim.dim_book_chapter_info    as a2
-        on a1.BookId = a2.book_id
+    select '${bf_1_dt}'               as dt
+          ,a1.bookid                  as book_id
+          ,a2.serial_number           as serial_number
+          ,max(a2.site_id)            as site_id
+          ,max(a2.new_cid)            as new_cid
+          ,max(a2.new_cname)          as new_cname
+          ,count(distinct a1.userid)  as chap5_read_num
+      from ods_log.ods_book_user_readchapter    as a1
+      join dim.dim_book_chapter_info            as a2
+        on a1.bookid = a2.book_id
        and a1.ChapterId = a2.chapter_id
-     where a1.dt >= date_sub('${bf_1_dt}',interval 6 day)
        and a1.dt <= '${bf_1_dt}'
-       and a2.serial_number = 5
-     group by 1,2,3
+       and a1.dt >= date_sub('${bf_1_dt}',interval 6 day)
+      left join dim.dim_shuangwen_book_read_consume_info as a3
+        on a1.bookid = a3.book_id
+     where a2.serial_number = 5
+       and a3.sexy2 < 4
+       and a2.site_id <> 333
+     group by 1, 2, 3
 )
-select dt
-      ,book_id
-      ,site_id
-      ,new_cid
-      ,new_cname
-      ,chap5_read_num
-      ,ttl_chap_read_num
-      ,sum(chap5_read_num)over(partition by dt, book_id, serial_number) as ttl_chap5_read_num
-from t1
+, t2 as (
+    select '${bf_1_dt}'               as dt
+          ,a1.bookid                  as book_id
+          ,count(distinct a1.userid)  as ttl_chap_read_num
+      from ods_log.ods_book_user_readchapter    as a1
+     inner join (select book_id
+                       ,chapter_id
+                   from dim.dim_book_chapter_info
+                  where site_id <> 333
+                )    as a2
+        on a1.bookid = a2.book_id
+       and a1.ChapterId = a2.chapter_id
+       and a1.dt <= '${bf_1_dt}'
+       and a1.dt >= date_sub('${bf_1_dt}',interval 6 day)
+     group by 1, 2
+)
+select t1.dt                      -- 日期
+      ,t1.book_id                 -- 书籍id
+      ,t1.site_id                 -- 语言id
+      ,t1.new_cid                 -- 分类id
+      ,t1.new_cname               -- 分类名称
+      ,t1.chap5_read_num          -- 第五章阅读人数
+      ,t2.ttl_chap_read_num       -- 总阅读人数
+      ,round(t1.chap5_read_num / t2.ttl_chap_read_num, 4) as chap5_ret_rt  -- 第五章留存率
+  from t1
+  left join t2
+    on t1.book_id = t2.book_id
+;
