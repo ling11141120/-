269f2eead42b2048f154dd9281a22c96b3035d77
王曦 <w17680412316@.163.com>
2025-11-06 20:38:19 +0800

    优化书籍第五章留存信息表的存储配置 修改表的分区策略为动态分区并调整相关参数 调整分布式存储配置，减少分桶数量 更新压缩和复制参数以优化性能

--- starrocks/ads/ddl/ads_bi_book_chap5_info.sql
+++ starrocks/ads/ddl/ads_bi_book_chap5_info.sql
@@ -1,23 +1,28 @@
 drop table if exists ads.ads_bi_book_chap5_ret_rt_info;
 create table ads.ads_bi_book_chap5_ret_rt_info (
-     dt                   date       not null comment '日期'
-    ,book_id              bigint     not null comment '书籍id'
-    ,site_id              int(11)    not null comment '语言id'
-    ,new_cid              int(11)             comment '分类id'
-    ,new_cname            varchar(100)        comment '分类名称'
-    ,chap5_read_num       int(11)             comment '第五章阅读人数'
-    ,ttl_chap_read_num    int(11)             comment '总阅读人数'
-    ,chap5_ret_rt         decimal(10,4)       comment '第五章留存率'
+     dt                   date      not null comment '日期'
+    ,book_id              bigint    not null comment '书籍id'
+    ,site_id              int(11)            comment '语言id'
+    ,new_cid              int(11)            comment '分类id'
+    ,new_cname            varchar(100)       comment '分类名称'
+    ,chap5_read_num       int(11)            comment '第五章阅读人数'
+    ,ttl_chap_read_num    int(11)            comment '总阅读人数'
+    ,chap5_ret_rt         decimal(10,4)      comment '第五章留存率'
 )
-primary key (dt, book_id,)
+primary key (dt,book_id)
 comment 'bi-书籍第5章留存信息表'
-partition by date_trunc('day', dt)
-distributed by hash(dt, book_id) buckets 8
+partition by range(dt)()
+distributed by hash(dt, book_id) buckets 1
 properties (
     "replication_num" = "3",
-    "in_memory" = "false",
-    "enable_persistent_index" = "true",
-    "replicated_storage" = "true",
+    "compression" = "LZ4",
+    "dynamic_partition.enable" = "true",
+    "dynamic_partition.time_unit" = "DAY",
+    "dynamic_partition.time_zone" = "Asia/Shanghai",
+    "dynamic_partition.start" = "-7",
+    "dynamic_partition.end" = "7",
+    "dynamic_partition.prefix" = "p",
+    "dynamic_partition.buckets" = "1",
     "compression" = "LZ4"
 )
 ;
