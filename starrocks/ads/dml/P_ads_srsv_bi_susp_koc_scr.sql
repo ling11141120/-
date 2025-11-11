8bf7d0b81976dde0c5eb2bd71382a00a9a7d8bbb
qhr <050239@mail.cd>
2025-11-11 18:45:09 +0800

    refactor(ads): 优化SQL查询逻辑并简化数据源处理 - 将数据插入目标表从ads改为tmp - 重命名列名使其更清晰表达含义 - 简化日期范围条件从365天改为1个月 - 使用coalesce函数处理空值并添加维度表关联 - 移除重复的条件判断逻辑

--- starrocks/ads/dml/P_ads_srsv_bi_susp_koc_scr.sql
+++ starrocks/ads/dml/P_ads_srsv_bi_susp_koc_scr.sql
@@ -6,13 +6,13 @@
 -- 开发日期： 2025-10-31
 -- 版本号： v0.0.0
 ----------------------------------------------------------------
-insert into ads.ads_srsv_bi_susp_koc_scr
+insert into tmp.ads_srsv_bi_susp_koc_scr
 with t1 as (
     select '${bf_1_dt}'    as dt
           ,a1.user_id
           ,a1.prj_type_cd
-          ,a2.total_episode_count
-          ,a3.avg_count    as min_avg_view_num
+          ,a2.ttl_view_num
+          ,a3.min_avg_view_num
           ,a1.pay_type     as pay_mth
           ,a4.is_sub       as tp_prd
       from (select user_id
@@ -22,28 +22,27 @@ with t1 as (
                           ,project_type
                           ,sub_pay_type
                       from ads.ads_srsv_trade_koc_payorderinfo_di
-                     where project_type = 2
+                     where dt >= date_sub('${bf_1_dt}', interval 1 month)
+                       and project_type = 2
                        and user_id is not null
                      group by 1, 2 ,3
                    )                                                          as b1
              group by 1, 2
            )                                                                  as a1
       left join (select login_id                                              as user_id
-                       ,count(distinct episode_id)                            as total_episode_count
+                       ,count(distinct episode_id)                            as ttl_view_num
                    from ods_log.ods_sensors_cd_video_startwatching
-                  where dt >= date_add('${bf_1_dt}',interval -365 day)
-                    and dt <= '${bf_1_dt}'
+                  where dt = '${bf_1_dt}'
                   group by 1
                 )                                                             as a2
         on a1.user_id = a2.user_id
       left join (select user_id
-                       ,round(avg(episode_count), 1) avg_count
+                       ,round(avg(episode_count), 1)                          as min_avg_view_num
                    from (select login_id                                      as user_id
                                ,date_format(event_tm, '%y-%m-%d %h:%i:00')    as minute_time
                                ,count(distinct episode_id)                    as episode_count
                            from ods_log.ods_sensors_cd_video_startwatching
-                          where dt >= date_add('${bf_1_dt}',interval -365 day)
-                            and dt <= '${bf_1_dt}'
+                          where dt = '${bf_1_dt}'
                           group by 1, 2
                         )                                                     as b2
                   group by 1
@@ -55,8 +54,7 @@ with t1 as (
                          end                                                  as is_sub
                       from ods_log.ods_sensors_cd_video_production_ordersuccess
                      where project_id = 8
-                       and dt >= date_add('${bf_1_dt}',interval -365 day)
-                       and dt <= '${bf_1_dt}'
+                       and dt = '${bf_1_dt}'
                      group by 1, 2
                     )                                                         as a4
         on a1.user_id = a4.user_id
@@ -64,8 +62,8 @@ with t1 as (
     select '${bf_1_dt}'                                                       as dt
           ,a1.user_id
           ,a1.prj_type_cd
-          ,a2.total_episode_count
-          ,a3.avg_count                                                       as min_avg_view_num
+          ,a2.ttl_view_num
+          ,a3.min_avg_view_num
           ,a1.pay_type                                                        as pay_mth
           ,a4.is_sub                                                          as tp_prd
       from (select user_id
@@ -75,28 +73,27 @@ with t1 as (
                           ,project_type
                           ,sub_pay_type
                       from ads.ads_srsv_trade_koc_payorderinfo_di
-                     where project_type = 1
+                     where dt >= date_sub('${bf_1_dt}', interval 1 month)
+                       and project_type = 1
                        and user_id is not null
                      group by 1, 2 ,3
                    )                                                          as b1
              group by 1, 2
            )                                                                  as a1
       left join (select login_id                                              as user_id
-                       ,count(distinct chapter_id)                            as total_episode_count
+                       ,count(distinct chapter_id)                            as ttl_view_num
                    from ods_log.ods_sensors_production_startreadingchapter
-                  where dt >= date_add('${bf_1_dt}',interval -365 day)
-                    and dt <= '${bf_1_dt}'
+                  where dt = '${bf_1_dt}'
                   group by 1
                 )                                                             as a2
         on a1.user_id = a2.user_id
       left join (select user_id
-                       ,round(avg(chapter_count), 1)                          as avg_count
+                       ,round(avg(chapter_count), 1)                          as min_avg_view_num
                    from (select login_id                                      as user_id
                                ,date_format(event_tm, '%y-%m-%d %h:%i:00')    as minute_time
                                ,count(distinct chapter_id)                    as chapter_count
                            from ods_log.ods_sensors_production_startreadingchapter
-                          where dt >= date_add('${bf_1_dt}',interval -365 day)
-                            and dt <= '${bf_1_dt}'
+                          where dt = '${bf_1_dt}'
                           group by 1, 2
                         )                                                     as b2
                   group by 1
@@ -107,30 +104,24 @@ with t1 as (
                              else '否'
                          end                                                  as is_sub
                    from ods_log.ods_sensors_cd_video_production_ordersuccess
-                  where project_id = 5
-                    and dt >= date_add('${bf_1_dt}',interval -365 day)
-                    and dt <= '${bf_1_dt}'
+                  where dt = '${bf_1_dt}'
+                    and project_id = 5
                   group by 1, 2
                 )                                                             as a4
         on a1.user_id = a4.user_id
 )
-select dt                            -- 日期
-      ,user_id                       -- 用户id
-      ,prj_type_cd                   -- 项目类型
-      ,case when prj_type_cd = 1 then '海阅'
-            when prj_type_cd = 2 then '海剧'
-            else '0'
-        end    as prj_type_name       -- 项目类型名称
-      ,case when total_episode_count >= 10 and min_avg_view_num < 1 then '1集/章/章以下'
-            when total_episode_count >= 10 and min_avg_view_num >= 1 and min_avg_view_num < 1.5 then '1-1.5集/章'
-            when total_episode_count >= 10 and min_avg_view_num >= 1.5 and min_avg_view_num < 2 then '1.5-2集/章'
-            when total_episode_count >= 10 and min_avg_view_num >= 2 and min_avg_view_num < 2.5 then '2-2.5集/章'
-            when total_episode_count >= 10 and min_avg_view_num >= 2.5 and min_avg_view_num < 3 then '2.5-3集/章'
-            when total_episode_count >= 10 and min_avg_view_num >= 3 and min_avg_view_num < 3.5 then '3-3.5集/章'
-            when total_episode_count >= 10 and min_avg_view_num >= 3.5 then '3.5集/章以上'
-            else '总观看集/章数少于10集/章'
-        end    as min_avg_view_num    -- 每分钟平均观看集/章数
-      ,pay_mth                        -- 支付方式
-      ,tp_prd                         -- 充值产品
-  from t1
+select a1.dt                                                  -- 日期
+      ,a1.user_id                                             -- 用户id
+      ,a1.prj_type_cd                                         -- 项目类型
+      ,coalesce(a2.cd_col_desc, '-99')    as prj_type_name    -- 项目类型名称
+      ,a1.ttl_view_num                                        -- 总观看集/章数
+      ,a1.min_avg_view_num                                    -- 每分钟平均观看集/章数
+      ,a1.pay_mth                                             -- 支付方式
+      ,a1.tp_prd                                              -- 充值产品
+  from t1                                    as a1
+  left join dim.dim_pub_code_mapping_dict    as a2
+    on a1.prj_type_cd = a2.cd_val
+   and a2.cd_col = 'biz_type_cd'
+   and a2.app_plat = 'pub'
+ where a1.ttl_view_num is not null
 ;
