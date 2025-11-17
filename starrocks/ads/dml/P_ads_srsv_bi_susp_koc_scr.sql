4ded10c44b415502b5edea96a729d4126b44a5bc
qhr <050239@mail.cd>
2025-11-17 15:55:36 +0800

    refactor(ads): 优化海剧海阅koc订单统计SQL查询结构
    重构SQL查询结构，将海剧和海阅的观看统计逻辑拆分为更清晰的子查询
    使用更一致的别名命名规范，提高代码可读性
    保持原有功能不变，仅优化查询结构

--- starrocks/ads/dml/P_ads_srsv_bi_susp_koc_scr.sql
+++ starrocks/ads/dml/P_ads_srsv_bi_susp_koc_scr.sql
@@ -20,66 +20,82 @@ select '${bf_1_dt}'                       as dt                  -- 日期
       ,a1.pay_type                        as pay_mth             -- 支付方式
       ,a4.is_sub                          as tp_prd              -- 充值产品
   from (select b1.user_id
-              ,b1.project_type                                                    as prj_type_cd
-              ,group_concat(b1.sub_pay_type)                                      as pay_type
-          from ads.ads_srsv_trade_koc_payorderinfo_di                             as b1    -- 海剧海阅koc订单
+              ,b1.project_type                                                                 as prj_type_cd
+              ,group_concat(b1.sub_pay_type)                                                   as pay_type
+          from ads.ads_srsv_trade_koc_payorderinfo_di                                          as b1    -- 海剧海阅koc订单
          where b1.dt >= date_sub('${bf_1_dt}', interval 1 year)
            and b1.dt <= '${bf_1_dt}'
            and b1.project_type in (1, 2)
            and b1.user_id is not null
          group by 1, 2
-       )                                                                          as a1
-  left join (select b2.login_id                                                   as user_id
-                   ,2                                                             as prj_type_cd
-                   ,count(distinct b2.episode_id)                                 as ttl_view_num
-                   ,round(avg(b2.episode_count), 1)                               as min_avg_view_num
-               from (select c1.login_id
-                           ,c1.episode_id
-                           ,date_trunc('minute', c1.event_tm)                     as min_tm
-                           ,count(distinct c1.episode_id)
-                             over(partition by c1.login_id
-                                              ,date_trunc('minute', c1.event_tm)
-                                 )    as episode_count
-                       from ods_log.ods_sensors_cd_video_startwatching            as c1    -- 海剧观看
+       )                                                                                       as a1
+  left join (
+             -- 海剧观看
+             select b2.user_id
+                   ,2                                                                          as prj_type_cd
+                   ,b2.ttl_view_num
+                   ,b3.min_avg_view_num
+               from (select c1.login_id                                                        as user_id
+                           ,count(distinct c1.episode_id)                                      as ttl_view_num
+                       from ods_log.ods_sensors_cd_video_startwatching                         as c1
                       where c1.dt = '${bf_1_dt}'
-                    )                                                             as b2
-              group by 1, 2
-            )                                                                     as a2
+                      group by 1
+                    )                                                                          as b2
+               left join (select c2.user_id
+                                ,round(avg(c2.episode_count), 1)                               as min_avg_view_num
+                            from (select d1.login_id                                           as user_id
+                                        ,date_trunc('minute', d1.event_tm)                     as min_tm
+                                        ,count(distinct d1.episode_id)                         as episode_count
+                                    from ods_log.ods_sensors_cd_video_startwatching            as d1
+                                   where d1.dt = '${bf_1_dt}'
+                                   group by 1, 2
+                                 )                                                             as c2
+                           group by 1
+                         )                                                                     as b3
+                 on b2.user_id = b3.user_id
+            )                                                                                  as a2
     on a1.user_id = a2.user_id
    and a1.prj_type_cd = a2.prj_type_cd
-  left join (select b3.login_id                                                   as user_id
-                   ,1                                                             as prj_type_cd
-                   ,count(distinct b3.episode_id)                                 as ttl_view_num
-                   ,round(avg(b3.episode_count), 1)                               as min_avg_view_num
-               from (select c2.login_id
-                           ,c2.episode_id
-                           ,date_trunc('minute', c2.event_tm)                     as min_tm
-                           ,count(distinct c2.episode_id)
-                             over(partition by c2.login_id
-                                              ,date_trunc('minute', c2.event_tm)
-                                 )    as episode_count
-                       from ods_log.ods_sensors_production_startreadingchapter    as c2    -- 海阅观看
-                      where c2.dt = '${bf_1_dt}'
-                    )                                                             as b3
-              group by 1, 2
-            )                                                                     as a3
+  left join (
+             -- 海阅观看
+             select b4.user_id
+                   ,1                                                                          as prj_type_cd
+                   ,b4.ttl_view_num
+                   ,b5.min_avg_view_num
+               from (select c3.login_id                                                        as user_id
+                           ,count(distinct c3.episode_id)                                      as ttl_view_num
+                       from ods_log.ods_sensors_production_startreadingchapter                 as c3
+                      where c3.dt = '${bf_1_dt}'
+                    )                                                                          as b4
+               left join (select c4.user_id
+                                ,round(avg(c4.episode_count), 1)                               as min_avg_view_num
+                            from (select d2.login_id                                           as user_id
+                                        ,date_trunc('minute', d2.event_tm)                     as min_tm
+                                        ,count(distinct d2.episode_id)                         as episode_count
+                                    from ods_log.ods_sensors_production_startreadingchapter    as d2
+                                   where d2.dt = '${bf_1_dt}'
+                                   group by 1, 2
+                                 )                                                             as c4
+                         )                                                                     as b5
+                  on b4.user_id = b5.user_id
+            )                                                                                  as a3
     on a1.user_id = a3.user_id
    and a1.prj_type_cd = a3.prj_type_cd
-  left join (select b4.login_id                                                   as user_id
-                   ,case when b4.project_id = 8 then 2
+  left join (select b6.login_id                                                                as user_id
+                   ,case when b6.project_id = 8 then 2
                          else 1
-                     end                                                          as prj_type_cd
-                   ,case when b4.subscription_days >= 1 then '是'
+                     end                                                                       as prj_type_cd
+                   ,case when b6.subscription_days >= 1 then '是'
                          else '否'
                      end        as is_sub
-              from ods_log.ods_sensors_cd_video_production_ordersuccess           as b4    -- 海剧海阅充值成功
-             where b4.project_id in (8, 5)
-               and b4.dt = '${bf_1_dt}'
+              from ods_log.ods_sensors_cd_video_production_ordersuccess                        as b6    -- 海剧海阅充值成功
+             where b6.project_id in (8, 5)
+               and b6.dt = '${bf_1_dt}'
              group by 1, 2, 3
-            )                                                                     as a4
+            )                                                                                  as a4
     on a1.user_id = a4.user_id
    and a1.prj_type_cd = a4.prj_type_cd
-  left join dim.dim_pub_code_mapping_dict                                         as a5
+  left join dim.dim_pub_code_mapping_dict                                                      as a5
     on a1.prj_type_cd = a5.cd_val
    and a5.cd_col = 'biz_type_cd'
    and a5.app_plat = 'pub'
