9e6cad63893d7014f79f42017b0ff0da9b3aa832
qhr <050239@mail.cd>
2026-02-24 17:12:58 +0800

    feat(dws): 更新短视频用户宽表活跃周期表结构和数据加载逻辑
    - 修改表结构中字段类型，将bigint(20)改为bigint，varchar(65533)改为string
    - 添加新的分区定义partition p202602 values less than ("2026-03-01")
    - 重新格式化SQL代码，改善代码可读性和维护性
    - 在DML脚本中添加程序头部注释信息，包括功能描述、负责人和开发日期
    - 优化CTE查询结构，提升数据处理效率
    - 添加对${dt}参数的支持，扩展数据加载的灵活性
    - 统一代码风格，规范化字段别名和查询语法

--- starrocks/dws/ddl/dws_user_short_video_wide_active_period_ed.sql
+++ starrocks/dws/ddl/dws_user_short_video_wide_active_period_ed.sql
@@ -2,33 +2,35 @@ drop table if exists dws.dws_user_short_video_wide_active_period_ed;
 create table dws.dws_user_short_video_wide_active_period_ed (
      dt                     date           not null comment "日期"
     ,period_type            varchar(20)    not null comment "统计周期类型,ctt/rmt,rmt(拉活用户)"
-    ,product_id             bigint(20)     not null comment "产品id"
-    ,user_id                varchar(65533) not null comment "用户id"
+    ,product_id             bigint         not null comment "产品id"
+    ,user_id                string         not null comment "用户id"
     ,user_type              varchar(100)            comment "用户类型"
-    ,corever                bigint(20)              comment "core"
-    ,mt                     bigint(20)              comment "mt"
-    ,current_language       bigint(20)              comment "当前语言"
-    ,current_language2      bigint(20)              comment "注册语言"
+    ,corever                bigint                  comment "core"
+    ,mt                     bigint                  comment "mt"
+    ,current_language       bigint                  comment "当前语言"
+    ,current_language2      bigint                  comment "注册语言"
     ,source_chl             varchar(1000)           comment "最新渠道"
-    ,reg_country            varchar(65533)          comment "注册国家"
-    ,country_level          varchar(65533)          comment "国家等级"
+    ,reg_country            string                  comment "注册国家"
+    ,country_level          string                  comment "国家等级"
     ,reg_time               datetime                comment "注册时间"
-    ,reg_days               bigint(20)              comment "活跃留存天数"
-    ,sex                    bigint(20)              comment "性别"
-    ,is_acc_login           smallint(6)             comment "是否登录用户（使用任一种三方账号登录、或设置密码的用户）"
-    ,is_has_email           smallint(6)             comment "是否拥有邮箱信息（使用任一种三方账号登录、或设置密码的用户）"
+    ,reg_days               bigint                  comment "活跃留存天数"
+    ,sex                    bigint                  comment "性别"
+    ,is_acc_login           smallint                comment "是否登录用户（使用任一种三方账号登录、或设置密码的用户）"
+    ,is_has_email           smallint                comment "是否拥有邮箱信息（使用任一种三方账号登录、或设置密码的用户）"
     ,popularize_series_code varchar(100)            comment "推广剧编号"
     ,etl_time               datetime                comment "数据清洗时间"
 )
 primary key(dt, period_type, product_id, user_id)
 comment "用户域登录观看充值消耗汇总活跃表"
 partition by range(dt)
+(partition p202602 values less than ("2026-03-01"))
 distributed by hash(product_id, user_id)
-properties ("replication_num" = "3",
-            "bloom_filter_columns" = "mt, reg_time, current_language2, current_language, reg_country, corever, period_type",
-            "in_memory" = "false",
-            "enable_persistent_index" = "true",
-            "replicated_storage" = "true",
-            "compression" = "LZ4"
+properties (
+    "replication_num" = "3",
+    "bloom_filter_columns" = "mt, reg_time, current_language2, current_language, reg_country, corever, period_type",
+    "in_memory" = "false",
+    "enable_persistent_index" = "true",
+    "replicated_storage" = "true",
+    "compression" = "LZ4"
 )
 ;
