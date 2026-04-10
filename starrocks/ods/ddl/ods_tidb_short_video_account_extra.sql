80c8ffa3ce9292ed0bafcf1b32ab9d52d49ea938
025716 <025716@mail.cd>
2026-04-10 13:38:38 +0800

    feat(ads/ods): 更新金币网赚用户视图和ODS表结构
    - 修改ads_user_gold_coin_user_view视图，增加DL用户相关字段和业务逻辑
    - 扩展ods_tidb_short_video_account_extra表结构，新增DL用户标识和货币码字段
    - 调整视图查询逻辑，使用row_number获取最新用户记录
    - 接入部分bi需求ods表单

--- starrocks/ods/ddl/ods_tidb_short_video_account_extra.sql
+++ starrocks/ods/ddl/ods_tidb_short_video_account_extra.sql
@@ -4,20 +4,25 @@
 -- 来源表： short_video.account_extra
 -- 来源负责：
 -- 采集工具： SeaTunnel
--- 开发人： roger
+-- 开发人： roger/xjc
 -- 开发日期：2025-12-15
 ----------------------------------------------------------------
 drop table if exists ods.ods_tidb_short_video_account_extra;
 create table ods.ods_tidb_short_video_account_extra (
-     dt            date        not null                   comment '日期'
-    ,Id            bigint(20)  not null                   comment '主键ID'
-    ,AccountId     bigint(20)  not null                   comment '账号ID'
-    ,CollectEmail  string      default null               comment '收集邮箱'
-    ,CreateTime    datetime    default null               comment '创建时间'
-    ,UpdateTime    datetime    default null               comment '修改时间'
-    ,IsGolden      tinyint(4)  default '0'                comment '是否金币网赚版本用户,0-否，1-是'
-    ,sr_createtime datetime    default current_timestamp  comment 'starrocks数据注入时间'
-    ,sr_updatetime datetime    default current_timestamp  comment 'starrocks数据更新时间'
+     dt                date        not null                   comment '日期'
+    ,Id                bigint(20)  not null                   comment '主键ID'
+    ,AccountId         bigint(20)  not null                   comment '账号ID'
+    ,CollectEmail      string                                 comment '收集邮箱'
+    ,CreateTime        datetime                               comment '创建时间'
+    ,UpdateTime        datetime                               comment '修改时间'
+    ,IsGolden          tinyint(4)                             comment '是否金币网赚版本用户,0-否，1-是'
+    ,sr_createtime     datetime    default current_timestamp  comment 'starrocks数据注入时间'
+    ,sr_updatetime     datetime    default current_timestamp  comment 'starrocks数据更新时间'
+    ,IsDl              tinyint(4)  not null                   comment '是否DL用户,0-否，1-是'
+    ,dlUpdateTime      datetime                               comment 'DL用户修改时间'
+    ,isCoinDl          tinyint(4)  not null                   comment '是否金币DL用户,0-否，1-是'
+    ,is_dl0_for_you    tinyint(4)  not null                   comment '是否C4 DL0跳转ForYou用户,0-否，1-是'
+    ,currencyCode      varchar(60)                            comment '网赚用户首次货币码'
 )
 primary key(dt, Id)
 comment "用户额外信息表"
@@ -32,4 +37,3 @@ properties (
     "compression" = "LZ4"
 )
 ;
-
