aef4395e690b1d4e9ec6b451ac974c9323b93c26
谭云刚 <243710@qq.com>
2026-05-07 16:52:36 +0800

    feat(ods):北斗离线人群包标签增加短剧类型

--- starrocks/ods/ddl/ods_tidb_realtimecrowd_sv_user_metrics.sql
+++ starrocks/ods/ddl/ods_tidb_realtimecrowd_sv_user_metrics.sql
@@ -9,21 +9,21 @@
 
 drop table if exists ods.ods_tidb_realtimecrowd_sv_user_metrics;
 create table ods.ods_tidb_realtimecrowd_sv_user_metrics (
-     id                   bigint      not null                       comment "用户Id"
-    ,metrics              json        not null                       comment "指标数据"
-    ,update_time          datetime                                   comment "更新时间"
-    ,EtlTime             datetime     default '2026-05-01 00:00:00'  comment "首次全量etl时间"
-    ,sr_createtime        datetime    default current_timestamp      comment "starrocks数据注入时间"
-    ,sr_updatetime        datetime    default current_timestamp      comment "starrocks数据更新时间"
+     id                   bigint      not null                        comment "用户Id"
+    ,metrics              json        not null                        comment "指标数据"
+    ,update_time          datetime                                    comment "更新时间"
+    ,EtlTime              datetime     default '2026-05-01 00:00:00'  comment "首次全量etl时间"
+    ,sr_createtime        datetime    default current_timestamp       comment "starrocks数据注入时间"
+    ,sr_updatetime        datetime    default current_timestamp       comment "starrocks数据更新时间"
 )
 primary key(id)
 comment "人群包用户指标表"
 distributed by hash(id) buckets 10
 properties (
     "replication_num" = "3"
-    ,"in_memory" = "false"
-    ,"enable_persistent_index" = "true"
-    ,"replicated_storage" = "true"
-    ,"compression" = "LZ4"
+   ,"in_memory" = "false"
+   ,"enable_persistent_index" = "true"
+   ,"replicated_storage" = "true"
+   ,"compression" = "LZ4"
 )
 ;
