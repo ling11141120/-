319f147afa8983d393a46d63169a4bc3cabb2703
凌维波 <206716@qq.com>
2026-05-22 18:18:52 +0800

    feat: 海阅海剧广告首次预加载ecpm链路优化

--- starrocks/dws/dml/P_dws_srsv_user_first_preload_info_df.sql
+++ starrocks/dws/dml/P_dws_srsv_user_first_preload_info_df.sql
@@ -11,95 +11,75 @@
 -- SQL语句
 insert into dws.dws_srsv_user_first_preload_info_df
 -- 海阅：增量，首次时间下优先取 REWARDED
-select
-    product_id
-   ,user_id
-   ,ad_type
-   ,ecpm as lst_preload_ecpm
-   ,fst_preload_time
-   ,now() as etl_time
-from (
-    select
-        product_id
-       ,user_id
-       ,ad_type
-       ,ecpm
-       ,create_time as fst_preload_time
-       ,row_number() over(
-            partition by product_id, user_id, ad_type
-            order by
-                create_time,
-                case when upper(ad_format) = 'REWARDED' then 0 else 1 end
-        ) as rn
-    from (
-        select
-            a.product_id
-           ,a.id as user_id
-           ,3 as ad_type
-           ,get_json_string(b.s0,'$.valueMicros') * 1000 as ecpm
-           ,get_json_string(b.s0,'$.adFormat') as ad_format
-           ,b.create_time
-        from (
-            select
-                product_id
-               ,user_id
-               ,s0
-               ,create_time
-            from ads.ads_user_commonactionlog_view
-            where Action = 'FirstPreloadEvent'
-              and dt >= '${bf_1_dt}'
-        ) b
-        inner join (
-            select
-                product_id
-               ,id
-            from dim.dim_user_account_info_view
-        ) a on a.product_id = b.product_id and a.id = b.user_id
-    ) t
-) t
-where rn = 1
+select product_id
+     , user_id
+     , ad_type
+     , ecpm             as lst_preload_ecpm
+     , fst_preload_time
+     , now()            as etl_time
+  from (select product_id
+             , user_id
+             , ad_type
+             , ecpm
+             , create_time                                                                                                                                    as fst_preload_time
+             , row_number() over( partition by product_id, user_id, ad_type order by create_time, case when upper(ad_format) = 'REWARDED' then 0 else 1 end ) as rn
+          from (select a.product_id
+                     , a.id                                          as user_id
+                     , 3                                             as ad_type
+                     , get_json_string(b.s0, '$.valueMicros') * 1000 as ecpm
+                     , get_json_string(b.s0, '$.adFormat')           as ad_format
+                     , b.create_time
+                  from (select productid                      as product_id
+                             , UserId                         as user_id
+                             , s0
+                             , CreateTime                     as create_time
+                          from ods_log.ods_readerlog_xx_log_commonactionlog
+                         where Action = 'FirstPreloadEvent'
+                           and dt >= '${bf_1_dt}'
+                       ) b
+                  inner join (select product_id
+                                   , id
+                                from dim.dim_user_account_info_view
+                             ) a
+                    on a.product_id = b.product_id
+                   and a.id = b.user_id
+               ) t
+       ) t
+ where rn = 1
 
 union all
 
-select
-    product_id
-   ,user_id
-   ,ad_type
-   ,max(ecpm) as lst_preload_ecpm
-   ,fst_preload_time
-   ,now() as etl_time
-from (
-    select
-        product_id
-       ,user_id
-       ,ad_type
-       ,ecpm
-       ,create_time as fst_preload_time
-    from (
-        select
-            a.product_id
-           ,a.user_id
-           ,b.ad_type
-           ,b.value_micros * 1000 as ecpm
-           ,b.create_time
-           ,min(b.create_time) over(partition by a.product_id, a.user_id, b.ad_type) as min_create_time
-        from (
-            select
-                account_id
-               ,ad_type
-               ,value_micros
-               ,create_time
-            from ads.ads_sv_advertise_ad_preload_revenue_di_view
-            where create_time >= '${bf_1_dt}'
-        ) b
-        inner join (
-            select
-                product_id
-               ,user_id
-            from dim.dim_short_video_user_accountinfo
-        ) a on a.user_id = b.account_id
-    ) t
-    where create_time = min_create_time
-) t
-group by product_id, user_id, ad_type, fst_preload_time
+select product_id
+     , user_id
+     , ad_type
+     , max(ecpm)        as lst_preload_ecpm
+     , fst_preload_time
+     , now()            as etl_time
+  from (select product_id
+             , user_id
+             , ad_type
+             , ecpm
+             , create_time as fst_preload_time
+          from (select a.product_id
+                     , a.user_id
+                     , b.ad_type
+                     , b.value_micros * 1000                                                    as ecpm
+                     , b.create_time
+                     , min(b.create_time) over(partition by a.product_id, a.user_id, b.ad_type) as min_create_time
+                  from (select AccountId              as account_id
+                             , adType                 as ad_type
+                             , ValueMicros            as value_micros
+                             , CreateTime             as create_time
+                          from ods.ods_tidb_sv_short_video_log_ad_preload_revenue_di
+                         where CreateTime >= '${bf_1_dt}'
+                       ) b
+                  inner join (select product_id
+                                   , user_id
+                                from dim.dim_short_video_user_accountinfo
+                             ) a
+                    on a.user_id = b.account_id
+               ) t
+         where create_time = min_create_time
+       ) t
+ group by product_id, user_id, ad_type, fst_preload_time
 ;
