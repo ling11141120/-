061b82227fe4b218f45bffe07c260ce17f20dc74
谢季昌 <025716@mail.cd>
2026-06-05 15:02:26 +0800

    feat: 修改YouTube 频道收入跑数周期，将ods直接引用改为引用dwd视图

--- starrocks/ads/dml/P_ads_bi_youtube_be_chl_rev_anal.sql
+++ starrocks/ads/dml/P_ads_bi_youtube_be_chl_rev_anal.sql
@@ -9,50 +9,36 @@
 
 insert into ads.ads_bi_youtube_be_chl_rev_anal
 -- youtube频道收入信息日更新表数据
-with youtubechannel_report as (
-    select id
-          ,infoid
-          ,channel_id
-          ,dt
-          ,estimated_revenue
-          ,estimated_ad_revenue
-          ,estimated_vip_revenue
-          ,estimated_minuteswatched
-          ,updatetime
-          ,sr_createtime
-          ,sr_updatetime
-      from ods.ods_tidb_cdmoney_report_tidb_cn_youtubechannel_report
-     where dt between '${bf_5_dt}' and '${dt}'
-)
-, youtubechannel_detail as (
-    select a1.Id
-          ,a1.infoid
+with youtubechannel_detail as (
+    select a1.id
+          ,a1.info_id
           ,a1.channel_id
-          ,coalesce(a2.channename,a1.channename)    as channename
-          ,a1.channename_api
+          ,coalesce(a2.channel_name,a1.channel_name)    as channel_name
+          ,a1.channel_name_api
           ,a1.subscriber_count
           ,a1.view_count
           ,a1.updatetime
           ,a1.sr_createtime
           ,a1.sr_updatetime
-      from ods.ods_tidb_cdmoney_report_tidb_cn_youtubechannel_detail    as a1
-      left join ods.ods_tidb_cdmoney_report_tidb_cn_youtube_info        as a2
-        on a1.Id=a2.id
+      from dwd.dwd_content_youtubechannel_detail_view    as a1
+      left join dwd.dwd_content_youtube_info_view        as a2
+        on a1.id=a2.id
        and a2.status=1
 )
 select a1.dt                          as dt                   -- 日期
       ,a1.channel_id                  as chl_id               -- 渠道id
-      ,a2.channename                  as chl_name             -- 渠道名称 展示用的
-      ,a2.channename_api              as chl_name_api         -- 渠道名称 api上取的
+      ,a2.channel_name                as chl_name             -- 渠道名称 展示用的
+      ,a2.channel_name_api            as chl_name_api         -- 渠道名称 api上取的
       ,a2.subscriber_count            as ttl_sub_num          -- 总订阅人数
       ,a2.view_count                  as ttl_view_num         -- 总观看次数
       ,a1.estimated_revenue           as d_ttl_rev            -- 当日总收入
       ,a1.estimated_ad_revenue        as d_ad_rev             -- 当日广告收入
-      ,a1.estimated_vip_revenue       as d_mbr_rev            -- 当日会员收入（近似值）
+      ,a1.estimated_vip_revenue       as d_mbr_apx_rev        -- 当日会员收入（近似值）
       ,a1.estimated_minuteswatched    as d_view_time          -- 当日观看时长 （分钟）
       ,a1.updatetime                  as d_rev_etl_time       -- 当日收入etl时间
       ,a2.updatetime                  as chl_info_etl_time    -- 渠道信息etl时间
-  from youtubechannel_report          as a1
+  from dwd.dwd_content_youtubechannel_report_view    as a1
   left join youtubechannel_detail     as a2
     on a1.channel_id = a2.channel_id
+ where a1.dt between '${bf_37_dt}' and '${dt}'
 ;
